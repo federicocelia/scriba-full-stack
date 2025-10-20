751cfc64f6a2dc5fc580a0bf48918ebca8dd58f5
Federico Celia <73991066+federicocelia@users.noreply.github.com>
2025-10-20 20:53:16 +0100

    adding a start game buttun and implementing fist functionality
    start game button and start game function initial implementation

--- blackJackApp/script.js
+++ blackJackApp/script.js
@@ -7,26 +7,18 @@ let isAlive = true;
 
 let message = "";
 
-console.log("Sum: " + sum);
-
-if (sum < 21) {
-  message = "Do you want to draw a new card? 🙂";
-  isAlive = true;
-} else if (sum === 21) {
-  message = "Wohoo! You've got Blackjack! 🤑";
-  hasBlackJack = true;
-  isAlive = true;
-} else {
-  message = "You're out of the game! 😞";
-  isAlive = false;
+function startGame() {
+  console.log("Sum: " + sum);
+  if (sum < 21) {
+    message = "Do you want to draw a new card? 🙂";
+    isAlive = true;
+  } else if (sum === 21) {
+    message = "Wohoo! You've got Blackjack! 🤑";
+    hasBlackJack = true;
+    isAlive = true;
+  } else {
+    message = "You're out of the game! 😞";
+    isAlive = false;
+  }
+  console.log(message);
 }
-
-console.log(message);
-
-// let age = 22;
-
-// if (age < 18) {
-//   console.log("You are a minor.");
-// } else {
-//   console.log("You are an adult.");
-// }
