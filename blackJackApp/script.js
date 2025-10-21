66d633a69c0d72b032ab368732f59b3c9f473ce7
Federico Celia <73991066+federicocelia@users.noreply.github.com>
2025-10-21 20:15:22 +0100

    new card buttun and initial functionality

--- blackJackApp/script.js
+++ blackJackApp/script.js
@@ -1,9 +1,11 @@
-let firstCard = 12;
-let secondCard = 10;
+let firstCard = 10;
+let secondCard = 8;
 let sum = firstCard + secondCard;
 
 let hasBlackJack = false;
 let isAlive = true;
+let newCardButton = document.querySelector("#new-card-button");
+newCardButton.style.display = "none";
 
 let message = "";
 let cards = "";
@@ -11,15 +13,22 @@ let cards = "";
 let messageEl = document.getElementById("message-el");
 let cardsEl = document.getElementById("cards-el");
 let sumEl = document.getElementById("sum-el");
-let startButton = document.getElementById("start-button");
+
+let startButton = document.querySelector("#start-button");
 
 function startGame() {
+  rendertGame();
+}
+
+function rendertGame() {
   console.log("Sum: " + sum);
   if (sum < 21) {
     message = "Do you want to draw a new card? 🙂";
+    newCardButton.style.display = "inline";
     isAlive = true;
   } else if (sum === 21) {
     message = "Wohoo! You've got Blackjack! 🤑";
+    newCardButton.style.display = "inline";
     hasBlackJack = true;
     isAlive = true;
   } else {
@@ -30,4 +39,11 @@ function startGame() {
   cardsEl.textContent = `Cards: ${firstCard} - ${secondCard}`;
   sumEl.textContent = `Sum: ${sum}`;
   startButton.textContent = "RESTART GAME";
+}
+
+function newCard() {
+  let thirdCard = 3;
+  sum += thirdCard;
+  startGame();
+  cardsEl.textContent = `Cards: ${firstCard} - ${secondCard} - ${thirdCard}`;
 }
